b3c9d152b1bc99c2661fe37067ccf01bb4da2257
Albert Łapa <103189133+NaiveDiamond7@users.noreply.github.com>
2024-12-27 19:55:47 +0100

    Added solved exercise 2 from lab8 "creational"

---
+++ lab8_zad2_CPP.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Vehicle {
+public:
+    virtual void setAttrib() = 0;
+    virtual int getWheels() = 0;
+    virtual bool getEngine() = 0;
+    virtual ~Vehicle() {}
+};
+
+class Car : public Vehicle {
+    int wheels;
+    bool engine;
+
+public:
+    void setAttrib() override {
+        wheels = 4;
+        engine = true;
+    }
+    int getWheels() override {
+        return wheels;
+    }
+    bool getEngine() override {
+        return engine;
+    }
+};
+
+class Bike : public Vehicle {
+    int wheels;
+    bool engine;
+
+public:
+    void setAttrib() override {
+        wheels = 2;
+        engine = false;
+    }
+    int getWheels() override {
+        return wheels;
+    }
+    bool getEngine() override {
+        return engine;
+    }
+};
+
+class VehicleFactory {
+public:
+    virtual Vehicle* createVehicle() = 0;
+    virtual ~VehicleFactory() {}
+};
+
+class CarFactory : public VehicleFactory {
+public:
+    Vehicle* createVehicle() override {
+        Vehicle* car = new Car();
+        car->setAttrib();
+        return car;
+    }
+};
+
+class BikeFactory : public VehicleFactory {
+public:
+    Vehicle* createVehicle() override {
+        Vehicle* bike = new Bike();
+        bike->setAttrib();
+        return bike;
+    }
+};
+
+int main() {
+
+    VehicleFactory* bikeFactory = new BikeFactory();
+    Vehicle* bike = bikeFactory->createVehicle();
+    cout << "Bike - Engine: " << bike->getEngine() << ", Wheels: " << bike->getWheels() << endl;
+
+    VehicleFactory* carFactory = new CarFactory();
+    Vehicle* car = carFactory->createVehicle();
+    cout << "Car - Engine: " << car->getEngine() << ", Wheels: " << car->getWheels() << endl;
+
+    delete bike;
+    delete bikeFactory;
+    delete car;
+    delete carFactory;
+
+    return 0;
+}
