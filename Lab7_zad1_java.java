94c8d016e55e0d4e00d4f898c90bd8559f6c9d54
Albert Łapa <103189133+NaiveDiamond7@users.noreply.github.com>
2025-01-18 23:10:18 +0100

    Added solved exercises from Lab7 solid in written in Java.
    These programs were previously done in C++.

---
+++ Lab7_zad1_java.java
@@ -0,0 +1,62 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
+// Klasa reprezentująca użytkownika
+class User {
+    private String name;
+    private int age;
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // Gettery
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+// Klasa odpowiedzialna za wyświetlanie danych użytkownika
+class UserDisplayer {
+    public void displayUser(User user) {
+        System.out.println("Name: " + user.getName() + ", Age: " + user.getAge());
+    }
+}
+
+// Klasa odpowiedzialna za zapisywanie danych użytkownika
+class UserSaver {
+    public void saveUser(User user, String filename) {
+        try (FileWriter writer = new FileWriter(filename, true)) {
+            writer.write("Name: " + user.getName() + ", Age: " + user.getAge() + "\n");
+            System.out.println("User saved to file: " + filename);
+        } catch (IOException e) {
+            System.err.println("Error: Could not save user to file " + filename);
+        }
+    }
+}
+
+public class Lab7_zad1_java {
+    public static void main(String[] args) {
+        User user = new User("John Doe", 30);
+        User user1 = new User("Czarek", 19);
+        User user2 = new User("Mati", 20);
+
+        UserDisplayer displayer = new UserDisplayer();
+        UserSaver saver = new UserSaver();
+
+        // Wyświetlanie użytkowników
+        displayer.displayUser(user);
+        displayer.displayUser(user1);
+        displayer.displayUser(user2);
+
+        // Zapisywanie użytkowników do pliku
+        saver.saveUser(user, "users.txt");
+        saver.saveUser(user1, "users.txt");
+        saver.saveUser(user2, "users.txt");
+    }
+}
---
+++ Lab7_zad2_java.java
@@ -0,0 +1,41 @@
+interface Discount {
+    double calculate(double price);
+}
+
+class RegularDiscount implements Discount {
+    @Override
+    public double calculate(double price) {
+        return price * 0.90; // 10% rabatu
+    }
+}
+
+class SeasonalDiscount implements Discount {
+    @Override
+    public double calculate(double price) {
+        return price * 0.80; // 20% rabatu
+    }
+}
+
+class DiscountApplier {
+    public void applyDiscount(Discount discount, double price) {
+        System.out.println("Original price: " + price);
+        System.out.println("Price after discount: " + discount.calculate(price));
+    }
+}
+
+public class Lab7_zad2_java {
+    public static void main(String[] args) {
+        double price = 100.0;
+
+        Discount regularDiscount = new RegularDiscount();
+        Discount seasonalDiscount = new SeasonalDiscount();
+
+        DiscountApplier discountApplier = new DiscountApplier();
+
+        System.out.println("Applying regular discount:");
+        discountApplier.applyDiscount(regularDiscount, price);
+
+        System.out.println("Applying seasonal discount:");
+        discountApplier.applyDiscount(seasonalDiscount, price);
+    }
+}
